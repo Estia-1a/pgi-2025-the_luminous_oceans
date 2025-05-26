18a192f4b11d33375d2824e72a5f813f2d94a5bc
ExpertGitGit <189098977+ExpertGitGit@users.noreply.github.com>
2025-05-26 12:19:52 +0200

    debut print pixelRGB

--- src/utils.h
+++ src/utils.h
@@ -12,8 +12,26 @@ typedef struct _pixelRGB {
   /*
    * TO COMPLETE
    */
-   char R ; 
+    unsigned char R;
+    unsigned char G;
+    unsigned char B;
 } pixelRGB ;
 
+pixelRGB * get_pixel( unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y ){
+    if (x >= width || y >= height) {
+        return NULL;
+    }
+    if (!data){
+      return NULL;
+    }
+    unsigned int index = (y * width + x) * n;
+    return (pixelRGB *) &data[index];
+}
+
+void setPixel(unsigned char* data, const unsigned int width, const unsigned int height, const unsigned int n, const unsigned int x, const unsigned int y, pixelRGB* pixel);
+
+int read_image_data(const char *filename, unsigned char **data, int *width, int *height, int *channel_count);
+
+void print_pixel(char *filename, int x, int y);
 
 #endif
