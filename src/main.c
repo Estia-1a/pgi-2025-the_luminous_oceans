a08676edbfc8827b236fb45a1e5bef697dc6ede6
Antoinelijeour <antoine.lijeour@etu.estia.fr>
2025-06-18 14:41:18 +0200

    color blue en trop

--- src/main.c
+++ src/main.c
@@ -76,9 +76,5 @@ int main(int argc, char **argv) {
     color_gray(configuration.filenames[0]);
   }
 
-  if (strncmp(configuration.command, "color_blue", 10) == 0) {
-    color_blue(configuration.filenames[0]);
-  }
-
   return 0;
 }
