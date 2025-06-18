a701ef85c7349ed68c66c866e20b39ab29f34b18
ExpertGitGit <189098977+ExpertGitGit@users.noreply.github.com>
2025-06-18 16:44:30 +0200

    fonction desaturate

--- src/features.h
+++ src/features.h
@@ -19,5 +19,6 @@ void rotate_acw(char *source_path);
 void mirror_vertical(char *source_path);
 void mirror_horizontal(char *source_path);
 void mirror_total(char *source_path);
+void color_desaturate(char *source_path);
 
 #endif
