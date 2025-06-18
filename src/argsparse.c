c7edfa14736c9fb0fb2e411c1899a58ae4d70483
Antoinelijeour <antoine.lijeour@etu.estia.fr>
2025-06-18 11:02:53 +0200

    RAZ config et argsparse

--- src/argsparse.c
+++ src/argsparse.c
@@ -6,13 +6,13 @@
 #include <stdlib.h>
 #include <string.h>
 //#include <unistd.h>
-
+ 
 #include "argsparse.h"
-
+ 
 static int file_count = 0 ;
 static int argument_count = 0 ;
 static int debug;
-
+ 
 void parse_arguments( int argc, char **argv, Config* conf ) {
   int c ;
   int length ;
@@ -26,78 +26,73 @@ void parse_arguments( int argc, char **argv, Config* conf ) {
       {"type",  required_argument, 0, 't'},
       {0, 0, 0, 0}
     };
-
+ 
+ 
   while (1) {
       int option_index = 0;
-
+ 
       c = getopt_long (argc, argv, "f:c:v:", long_options, &option_index);
-      conf->debug_mode = debug;
-
+      conf->debug_mode=debug;
+      /* Detect the end of the options. */
       if (c == -1) {
         break;
       }
       switch (c) {
         case 0 : break ;
         case 'f':
-          length = strlen(optarg) + 1;
-          conf->filenames[file_count] = malloc(length);
-          strncpy(conf->filenames[file_count], optarg, length);
-          file_count++;
+          length = strlen( optarg ) + 1 ;
+          conf->filenames[ file_count ] = malloc( length ) ;
+          strncpy( conf->filenames[ file_count ], optarg, length ) ;
+          file_count = file_count + 1 ;
           break;
         case 'c':
-          strncpy(conf->command, optarg, MAX_LENGTH_COMMAND);
+          strncpy( conf->command, optarg, MAX_LENGTH_COMMAND ) ;
           break;
         case 't':
+ 
           break;
         case '?':
+          /* getopt_long already printed an error message. */
           break;
         case 'v':
           puts("Version 1.0.0");
           exit(0);
+          break;
         default:
-          printf("option %s unhandled", long_options[option_index].name);
-          abort();
-      }
-  }
-
-  // Récupère les arguments restants (positionnels)
-  while (optind < argc && argument_count < MAX_ARGUMENT_COUNT) {
-    length = strlen(argv[optind]) + 1;
-    conf->arguments[argument_count] = malloc(length);
-    strncpy(conf->arguments[argument_count], argv[optind], length);
-    argument_count++;
-    optind++;
-  }
-
-  // Gestion spécifique pour print_pixel : x et y
-  if (strcmp(conf->command, "print_pixel") == 0) {
-    if (argument_count < 2) {
-        fprintf(stderr, "Erreur : commande print_pixel attend deux arguments (x y)\n");
-        exit(1);
+          printf ("option %s unhandled", long_options[option_index].name);
+          abort ();
+        }
     }
-    conf->x = atoi(conf->arguments[0]);
-    conf->y = atoi(conf->arguments[1]);
-  }
+    /* Print any remaining command line arguments (not options). */
+    if (optind < argc) {
+        while (optind < argc) {
+          length = strlen( argv[optind] ) + 1 ;
+          conf->arguments[ argument_count ] = malloc( length ) ;
+          strncpy( conf->arguments[ argument_count ], argv[optind], length ) ;
+          argument_count = argument_count + 1 ;
+          optind = optind + 1 ;
+        }
+      }
 }
-
+ 
 void check_file() {
-    if (file_count < 1) {
-        printf("Missing file\n");
+    if ( file_count < 1 ) {
+        printf("Missing file\n" );
         abort();
     }
 }
-
+ 
 void check_debug_mode(Config conf) {
     if (conf.debug_mode) {
         printf("debug flag is set");
-        int file_index;
-        for (file_index = 0; file_index < file_count; file_index++) {
-            printf("file %d/%d: %s\n", file_index + 1, file_count, conf.filenames[file_index]);
+        int file_index ;
+        for ( file_index = 0 ; file_index < file_count ; file_index++ ) {
+            printf( "file %d/%d: %s\n", file_index + 1 , file_count, conf.filenames[ file_index ] );
         }
-        int argument_index;
-        for (argument_index = 0; argument_index < argument_count; argument_index++) {
-            printf("arguments %d/%d: %s\n", argument_index + 1, argument_count, conf.arguments[argument_index]);
+        int argument_index ;
+        for ( argument_index = 0 ; argument_index < argument_count ; argument_index++ ) {
+            printf( "arguments %d/%d: %s\n", argument_index + 1 , argument_count, conf.arguments[ argument_index ] );
         }
-        printf("option: %s\n", conf.command);
-    }
+        printf( "option: %s\n", conf.command );
+  }
 }
--- src/configp.h
+++ src/configp.h
@@ -3,24 +3,18 @@
 */
 #ifndef CONFIGP_H
 #define CONFIGP_H
-
-
+ 
 #include <stdio.h>
-
+ 
 #define MAX_FILE_COUNT 10 /* Maximum number of files */
 #define MAX_LENGTH_COMMAND 25 /* Maximum length of a command */
 #define MAX_ARGUMENT_COUNT 5 /* Maximum number of arguments */
-
+ 
 typedef struct _config {
-    int debug_mode;
-    char command[MAX_LENGTH_COMMAND];
-    char* filenames[MAX_FILE_COUNT];
-    char* arguments[MAX_ARGUMENT_COUNT];
-    
-    // ➕ Ajoute ceci :
-    int x;
-    int y;
-} Config;
-
-
+    int debug_mode ; /* Debug mode flag --debug */
+    char command[MAX_LENGTH_COMMAND] ; /* Command called. Example: helloworld */
+    char* filenames[MAX_FILE_COUNT] ; /* Images path. Example: ./images/input/image.jpeg */
+    char* arguments[MAX_ARGUMENT_COUNT] ; /* Other arguments. */
+} Config ;
+ 
 #endif
